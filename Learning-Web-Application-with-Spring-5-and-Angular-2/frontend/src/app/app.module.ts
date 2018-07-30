63185e6e0c8075864aaef39addadf34a513c676e
SergiOn <onishhenko@gmail.com>
2018-07-30 23:11:38 +0300

    #bwaw-spring-framework-and-angular-2 # section 3, lecture 8 # Add Navigation Bar - Frontend

--- Learning-Web-Application-with-Spring-5-and-Angular-2/frontend/src/app/app.module.ts
+++ Learning-Web-Application-with-Spring-5-and-Angular-2/frontend/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
+import { routing } from './app.routing';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { PhotoListComponent } from './components/photo-list/photo-list.component';
 import { PhotoService } from './services/photo.service';
 import { SidePanelComponent } from './components/side-panel/side-panel.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 
 @NgModule({
@@ -16,11 +18,13 @@ import { SidePanelComponent } from './components/side-panel/side-panel.component
     HomeComponent,
     PhotoListComponent,
     SidePanelComponent,
+    NavBarComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
+    routing,
   ],
   providers: [
     PhotoService,
