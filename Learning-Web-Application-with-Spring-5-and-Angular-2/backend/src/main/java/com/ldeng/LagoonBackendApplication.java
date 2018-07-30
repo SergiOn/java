ae4ace210fa543c700d8f1e0bb304e1559dcdf5a
SergiOn <onishhenko@gmail.com>
2018-07-31 00:18:49 +0300

    #bwaw-spring-framework-and-angular-2 # section 3, lecture 9 # Add Photo Model - Backend

---
+++ Learning-Web-Application-with-Spring-5-and-Angular-2/backend/src/main/java/com/ldeng/LagoonBackendApplication.java
@@ -0,0 +1,12 @@
+package com.ldeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LagoonBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LagoonBackendApplication.class, args);
+	}
+}
---
+++ Learning-Web-Application-with-Spring-5-and-Angular-2/backend/src/main/java/com/ldeng/model/Photo.java
@@ -0,0 +1,104 @@
+package com.ldeng.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Photo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long photoId;
+
+    private String photoName;
+    private String title;
+    private String description;
+    private String imageName;
+
+    @CreationTimestamp
+    private Date created;
+
+    @ManyToOne
+    @JsonBackReference
+    private User user;
+
+    private int likes;
+
+    @OneToMany(mappedBy = "photo", fetch = FetchType.EAGER)
+    private List<Comment> commentList;
+
+    public Long getPhotoId() {
+        return photoId;
+    }
+
+    public void setPhotoId(Long photoId) {
+        this.photoId = photoId;
+    }
+
+    public String getPhotoName() {
+        return photoName;
+    }
+
+    public void setPhotoName(String photoName) {
+        this.photoName = photoName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageName() {
+        return imageName;
+    }
+
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+    public List<Comment> getCommentList() {
+        return commentList;
+    }
+
+    public void setCommentList(List<Comment> commentList) {
+        this.commentList = commentList;
+    }
+}
