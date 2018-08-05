35dece8920c2e0e29370719f606876e9ea3218e7
SergiOn <onishhenko@gmail.com>
2018-08-05 23:03:16 +0300

    #bwaw-spring-framework-and-angular-2 # section 4, lecture 20 # Add Login Function - Backend

--- Learning-Web-Application-with-Spring-5-and-Angular-2/backend/src/main/java/com/ldeng/controller/UserController.java
+++ Learning-Web-Application-with-Spring-5-and-Angular-2/backend/src/main/java/com/ldeng/controller/UserController.java
@@ -2,11 +2,17 @@ package com.ldeng.controller;
 
 import com.ldeng.model.User;
 import com.ldeng.service.UserService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.ServletException;
+import java.util.Date;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
@@ -15,7 +21,35 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-//    public String login() {}
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String login(@RequestBody Map<String, String> json) throws ServletException {
+
+        if (json.get("username") == null || json.get("password") == null) {
+            throw new ServletException("Please fill in username and password");
+        }
+
+        String username = json.get("username");
+        String password = json.get("password");
+
+        User user = userService.findByUserName(username);
+
+        if (user == null) {
+            throw new ServletException("Username not found");
+        }
+
+        String pwd = user.getPassword();
+
+        if (!password.equals(pwd)) {
+            throw new ServletException("Invalid login. Please check your username and password");
+        }
+
+        return Jwts.builder()
+                .setSubject(username)
+                .claim("roles", "user")
+                .setIssuedAt(new Date())
+                .signWith(SignatureAlgorithm.HS256, "secretKey")
+                .compact();
+    }
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
     public User registerUser(@RequestBody User user) {
