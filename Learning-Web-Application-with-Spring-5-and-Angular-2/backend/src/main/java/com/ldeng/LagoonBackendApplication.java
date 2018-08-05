36058427393f833fe36a99e1d4dfd9872d01706e
SergiOn <onishhenko@gmail.com>
2018-08-05 17:45:27 +0300

    #bwaw-spring-framework-and-angular-2 # section 4, lecture 17 # Add JWT Filters - Backend

--- Learning-Web-Application-with-Spring-5-and-Angular-2/backend/src/main/java/com/ldeng/LagoonBackendApplication.java
+++ Learning-Web-Application-with-Spring-5-and-Angular-2/backend/src/main/java/com/ldeng/LagoonBackendApplication.java
@@ -1,10 +1,22 @@
 package com.ldeng;
 
+import com.ldeng.config.JwtFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LagoonBackendApplication {
+
+	@Bean
+	public FilterRegistrationBean<JwtFilter> jwtFilter() {
+
+		final FilterRegistrationBean<JwtFilter> registrationBean = new FilterRegistrationBean<>();
+		registrationBean.setFilter(new JwtFilter());
+		registrationBean.addUrlPatterns("/rest/*");
+		return registrationBean;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LagoonBackendApplication.class, args);
