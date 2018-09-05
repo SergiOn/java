3ca5800bc1ebb8ad54b733dbd9ce1261b627d8dc
SergiOn <onishhenko@gmail.com>
2018-09-05 15:45:40 +0300

    #lwa-spring-5-angular-2 # section 1, lecture 3 # Creating an Angular 2 Components

--- Learning-Web-Application-with-Spring-5-and-Angular-2/login-app/src/app/app.module.ts
+++ Learning-Web-Application-with-Spring-5-and-Angular-2/login-app/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { LoginModule } from './login/login.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    LoginModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
---
+++ Learning-Web-Application-with-Spring-5-and-Angular-2/login-app/src/app/login/login.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthenticationComponent } from './authentication/authentication.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+  ],
+  exports: [
+    AuthenticationComponent,
+  ],
+  declarations: [
+    AuthenticationComponent,
+  ],
+})
+export class LoginModule { }
