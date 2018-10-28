93f0ed7d9255a04b8590e216f4c608978a6f2b09
SergiOn <onishhenko@gmail.com>
2018-10-28 15:20:33 +0200

    #reactive-programming-spring-5 # section 3, lecture 16 # Initializing Data With Spring Boot Command Line Runner

---
+++ Reactive-Programming-with-Spring-Framework-5/netflux-example/src/main/java/guru/springframework/netfluxexample/bootstrap/BootstrapCLR.java
@@ -0,0 +1,33 @@
+package guru.springframework.netfluxexample.bootstrap;
+
+import guru.springframework.netfluxexample.domain.Movie;
+import guru.springframework.netfluxexample.repositories.MovieRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+
+import java.util.UUID;
+
+@Component
+public class BootstrapCLR implements CommandLineRunner {
+
+    private final MovieRepository movieRepository;
+
+    public BootstrapCLR(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        //clear old data
+        movieRepository.deleteAll()
+                .thenMany(
+                        Flux.just("Silence of the Lambdas", "AEon Flux", "Enter the Mono<Void>", "The Fluxxinator",
+                                "Back to the Future", "Meet the Fluxes", "Lord of the Fluxes")
+                                .map(title -> new Movie(UUID.randomUUID().toString(), title))
+                                .flatMap(movieRepository::save))
+                .subscribe(null, null, () -> {
+                    movieRepository.findAll().subscribe(System.out::println);
+                });
+    }
+}
