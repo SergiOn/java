385136497f957b9872160e4c6bf5e37b5d93a8cf
SergiOn <onishhenko@gmail.com>
2018-10-31 16:19:14 +0200

    #reactive-programming-spring-5 # section 7, lecture 48 # QuoteRunner

---
+++ Reactive-Programming-with-Spring-Framework-5/mongodb-reactive-stock-quote-service/src/main/java/guru/springframework/mongodbreactivestockquoteservice/QuoteRunner.java
@@ -0,0 +1,23 @@
+package guru.springframework.mongodbreactivestockquoteservice;
+
+import guru.springframework.mongodbreactivestockquoteservice.client.StockQuoteClient;
+import guru.springframework.mongodbreactivestockquoteservice.domain.Quote;
+import org.springframework.boot.CommandLineRunner;
+import reactor.core.publisher.Flux;
+
+//@Component
+public class QuoteRunner implements CommandLineRunner {
+
+    private final StockQuoteClient stockQuoteClient;
+
+    public QuoteRunner(StockQuoteClient stockQuoteClient) {
+        this.stockQuoteClient = stockQuoteClient;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Flux<Quote> quoteFlux = stockQuoteClient.getQuoteStream();
+
+        quoteFlux.subscribe(System.out::println);
+    }
+}
