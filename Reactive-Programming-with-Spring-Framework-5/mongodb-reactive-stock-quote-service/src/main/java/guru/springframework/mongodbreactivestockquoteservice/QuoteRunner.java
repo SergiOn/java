c678cb03344823b9d4552371c639a50d8a03a165
SergiOn <onishhenko@gmail.com>
2018-10-31 23:39:08 +0200

    #reactive-programming-spring-5 # section 7, lecture 51 # Tailable Cursors with MongoDB

--- Reactive-Programming-with-Spring-Framework-5/mongodb-reactive-stock-quote-service/src/main/java/guru/springframework/mongodbreactivestockquoteservice/QuoteRunner.java
+++ Reactive-Programming-with-Spring-Framework-5/mongodb-reactive-stock-quote-service/src/main/java/guru/springframework/mongodbreactivestockquoteservice/QuoteRunner.java
@@ -2,22 +2,49 @@ package guru.springframework.mongodbreactivestockquoteservice;
 
 import guru.springframework.mongodbreactivestockquoteservice.client.StockQuoteClient;
 import guru.springframework.mongodbreactivestockquoteservice.domain.Quote;
+import guru.springframework.mongodbreactivestockquoteservice.repositories.QuoteRepository;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
 
-//@Component
+@Component
 public class QuoteRunner implements CommandLineRunner {
 
     private final StockQuoteClient stockQuoteClient;
+    private final QuoteRepository repository;
 
-    public QuoteRunner(StockQuoteClient stockQuoteClient) {
+    public QuoteRunner(StockQuoteClient stockQuoteClient, QuoteRepository repository) {
         this.stockQuoteClient = stockQuoteClient;
+        this.repository = repository;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        Flux<Quote> quoteFlux = stockQuoteClient.getQuoteStream();
 
-        quoteFlux.subscribe(System.out::println);
+        Flux<Quote> quoteFlux = repository.findWithTailableCursorBy();
+
+        Disposable disposable = quoteFlux.subscribe(quote -> {
+            System.out.println("*#*#*#*#*#*#*#*#*#*#*#*#* Id: " + quote.getId());
+        });
+
+        disposable.dispose();
     }
 }
+
+////@Component
+//public class QuoteRunner implements CommandLineRunner {
+//
+//    private final StockQuoteClient stockQuoteClient;
+//
+//    public QuoteRunner(StockQuoteClient stockQuoteClient) {
+//        this.stockQuoteClient = stockQuoteClient;
+//    }
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//        Flux<Quote> quoteFlux = stockQuoteClient.getQuoteStream();
+//
+//        quoteFlux.subscribe(System.out::println);
+//    }
+//}
--- Reactive-Programming-with-Spring-Framework-5/mongodb-reactive-stock-quote-service/src/main/java/guru/springframework/mongodbreactivestockquoteservice/repositories/QuoteRepository.java
+++ Reactive-Programming-with-Spring-Framework-5/mongodb-reactive-stock-quote-service/src/main/java/guru/springframework/mongodbreactivestockquoteservice/repositories/QuoteRepository.java
@@ -2,6 +2,11 @@ package guru.springframework.mongodbreactivestockquoteservice.repositories;
 
 import guru.springframework.mongodbreactivestockquoteservice.domain.Quote;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.data.mongodb.repository.Tailable;
+import reactor.core.publisher.Flux;
 
 public interface QuoteRepository extends ReactiveMongoRepository<Quote, String> {
+
+    @Tailable
+    Flux<Quote> findWithTailableCursorBy(); //must be capped collection
 }
