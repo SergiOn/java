819d0d5ff2069ff32bb3d7dc763b7992703650db
SergiOn <onishhenko@gmail.com>
2018-11-18 23:13:45 +0200

    #online-bank-java-angular-spring # section 3, lecture 14 # Add Signup Component

--- Build-An-Online-Bank-With-Java-Angular-2-Spring-and-More/UserFront/src/main/java/com/userfront/controller/HomeController.java
+++ Build-An-Online-Bank-With-Java-Angular-2-Spring-and-More/UserFront/src/main/java/com/userfront/controller/HomeController.java
@@ -1,7 +1,11 @@
 package com.userfront.controller;
 
+import com.userfront.domain.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
@@ -14,6 +18,37 @@ public class HomeController {
     @RequestMapping("/index")
     public String index() {
         return "index";
+    }
+
+    @RequestMapping(value = "/signup", method = RequestMethod.GET)
+    public String signup(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
+        return "signup";
+    }
+
+    @RequestMapping(value = "/signup", method = RequestMethod.POST)
+    public /*String*/ void signupPost(@ModelAttribute("user") User user, Model model) {
+
+//        if(userService.checkUserExists(user.getUsername(), user.getEmail()))  {
+//
+//            if (userService.checkEmailExists(user.getEmail())) {
+//                model.addAttribute("emailExists", true);
+//            }
+//
+//            if (userService.checkUsernameExists(user.getUsername())) {
+//                model.addAttribute("usernameExists", true);
+//            }
+//
+//            return "signup";
+//        } else {
+//            Set<UserRole> userRoles = new HashSet<>();
+//            userRoles.add(new UserRole(user, roleDao.findByName("ROLE_USER")));
+//
+//            userService.createUser(user, userRoles);
+//
+//            return "redirect:/";
+//        }
     }
 
 }
