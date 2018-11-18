a164210bc92d9f36bf2508725cc080c036e3a68b
SergiOn <onishhenko@gmail.com>
2018-11-18 14:26:17 +0200

    #online-bank-java-angular-spring # section 3, lecture 10 # Add Domain Models - Part 1

---
+++ Build-An-Online-Bank-With-Java-Angular-2-Spring-and-More/UserFront/src/main/java/com/userfront/domain/PrimaryAccount.java
@@ -0,0 +1,44 @@
+package com.userfront.domain;
+
+import java.math.BigDecimal;
+
+public class PrimaryAccount {
+
+    private Long id;
+    private int accountNumber;
+    private BigDecimal accountBalance;
+
+    private List<PrimaryTransaction> primaryTransactionList;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public BigDecimal getAccountBalance() {
+        return accountBalance;
+    }
+
+    public void setAccountBalance(BigDecimal accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+
+    public List<PrimaryTransaction> getPrimaryTransactionList() {
+        return primaryTransactionList;
+    }
+
+    public void setPrimaryTransactionList(List<PrimaryTransaction> primaryTransactionList) {
+        this.primaryTransactionList = primaryTransactionList;
+    }
+}
---
+++ Build-An-Online-Bank-With-Java-Angular-2-Spring-and-More/UserFront/src/main/java/com/userfront/domain/Savings.java
@@ -0,0 +1,46 @@
+package com.userfront.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.math.BigDecimal;
+
+public class Savings {
+
+    private Long id;
+    private int accountNumber;
+    private BigDecimal accountBalance;
+
+    private List<SavingsTransaction> savingsTransactionList;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public BigDecimal getAccountBalance() {
+        return accountBalance;
+    }
+
+    public void setAccountBalance(BigDecimal accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+
+    public List<SavingsTransaction> getSavingsTransactionList() {
+        return savingsTransactionList;
+    }
+
+    public void setSavingsTransactionList(List<SavingsTransaction> savingsTransactionList) {
+        this.savingsTransactionList = savingsTransactionList;
+    }
+}
