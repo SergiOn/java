86c0fdb65bb0e08ad3031cf588559b86eed2c1d7
SergiOn <onishhenko@gmail.com>
2018-11-18 14:25:21 +0200

    #online-bank-java-angular-spring # section 3, lecture 10 # Generated project UserFront

---
+++ Build-An-Online-Bank-With-Java-Angular-2-Spring-and-More/UserFront/src/main/java/com/userfront/UserfrontApplication.java
@@ -0,0 +1,12 @@
+package com.userfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserfrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserfrontApplication.class, args);
+    }
+}
