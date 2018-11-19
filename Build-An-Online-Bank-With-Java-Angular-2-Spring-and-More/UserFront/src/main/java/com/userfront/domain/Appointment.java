01715f070667bfa271456ba600a0920b7c6af34c
SergiOn <onishhenko@gmail.com>
2018-11-19 22:48:24 +0200

    #online-bank-java-angular-spring # section 3, lecture 15 # Add Persistence Layer

--- Build-An-Online-Bank-With-Java-Angular-2-Spring-and-More/UserFront/src/main/java/com/userfront/domain/Appointment.java
+++ Build-An-Online-Bank-With-Java-Angular-2-Spring-and-More/UserFront/src/main/java/com/userfront/domain/Appointment.java
@@ -1,15 +1,21 @@
 package com.userfront.domain;
 
+import javax.persistence.*;
 import java.util.Date;
 
+@Entity
 public class Appointment {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private Date date;
     private String location;
     private String description;
     private boolean confirmed;
 
+    @ManyToOne
+    @JoinColumn(name = "user_id")
     private User user;
 
     public Long getId() {
@@ -58,5 +64,16 @@ public class Appointment {
 
     public void setUser(User user) {
         this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Appointment{" +
+                "id=" + id +
+                ", date=" + date +
+                ", location='" + location + '\'' +
+                ", description='" + description + '\'' +
+                ", user=" + user +
+                '}';
     }
 }
--- Build-An-Online-Bank-With-Java-Angular-2-Spring-and-More/UserFront/src/main/java/com/userfront/domain/PrimaryAccount.java
+++ Build-An-Online-Bank-With-Java-Angular-2-Spring-and-More/UserFront/src/main/java/com/userfront/domain/PrimaryAccount.java
@@ -1,14 +1,21 @@
 package com.userfront.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.util.List;
 
 public class PrimaryAccount {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private int accountNumber;
     private BigDecimal accountBalance;
 
+    @OneToMany(mappedBy = "primaryAccount", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JsonIgnore // it brakes the loop
     private List<PrimaryTransaction> primaryTransactionList;
 
     public Long getId() {
