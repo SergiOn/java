1a3ff001ae6870eb51db55b402caee0150f66e83
SergiOn <onishhenko@gmail.com>
2018-11-27 20:50:01 +0200

    #online-bank-java-angular-spring # section 4, lecture 22 # Add Spring Security for Login - Part 5

--- Build-An-Online-Bank-With-Java-Angular-2-Spring-and-More/UserFront/src/main/java/com/userfront/controller/HomeController.java
+++ Build-An-Online-Bank-With-Java-Angular-2-Spring-and-More/UserFront/src/main/java/com/userfront/controller/HomeController.java
@@ -59,7 +59,7 @@ public class HomeController {
         } else {
 
             Set<UserRole> userRoles = new HashSet<>();
-            userRoles.add(new UserRole(user, roleDao.findByName("USER")));
+            userRoles.add(new UserRole(user, roleDao.findByName("ROLE_USER")));
 
             userService.createUser(user, userRoles);
 
