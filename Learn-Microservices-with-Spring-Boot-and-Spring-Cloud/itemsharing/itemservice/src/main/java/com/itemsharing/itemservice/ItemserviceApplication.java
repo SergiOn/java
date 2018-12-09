f65161ce08b0c934e5f7cb9f93bbd02342e3a17d
SergiOn <onishhenko@gmail.com>
2018-12-09 20:51:17 +0200

    #learn-microservices-spring-boot-spring-cloud # section 4, lecture 16 # Adding Configuration Repository on Git

--- Learn-Microservices-with-Spring-Boot-and-Spring-Cloud/itemsharing/itemservice/src/main/java/com/itemsharing/itemservice/ItemserviceApplication.java
+++ Learn-Microservices-with-Spring-Boot-and-Spring-Cloud/itemsharing/itemservice/src/main/java/com/itemsharing/itemservice/ItemserviceApplication.java
@@ -1,12 +1,52 @@
 package com.itemsharing.itemservice;
 
+import com.itemsharing.itemservice.model.Item;
+import com.itemsharing.itemservice.model.User;
+import com.itemsharing.itemservice.service.ItemService;
+import com.itemsharing.itemservice.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Date;
+
 @SpringBootApplication
-public class ItemserviceApplication {
+public class ItemserviceApplication implements CommandLineRunner {
+
+	private ItemService itemService;
+	private UserService userService;
+
+	public ItemserviceApplication(ItemService itemService, UserService userService) {
+		this.itemService = itemService;
+		this.userService = userService;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ItemserviceApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		User user = userService.findByUsername("SerhiiOn");
+
+		Item item1 = new Item();
+		item1.setName("Item1");
+		item1.setItemCondition("New");
+		item1.setStatus("Active");
+		item1.setAddDate(new Date());
+		item1.setDescription("This is an item description.");
+		item1.setUser(user);
+
+		itemService.addItemByUser(item1, user.getUsername());
+
+		Item item2 = new Item();
+		item2.setName("Item2");
+		item2.setItemCondition("Used");
+		item2.setStatus("Inactive");
+		item2.setAddDate(new Date());
+		item2.setDescription("This is an item description for item2.");
+		item2.setUser(user);
+
+		itemService.addItemByUser(item2, user.getUsername());
 	}
 }
--- Learn-Microservices-with-Spring-Boot-and-Spring-Cloud/itemsharing/userservice/src/main/java/com/itemsharing/userservice/UserserviceApplication.java
+++ Learn-Microservices-with-Spring-Boot-and-Spring-Cloud/itemsharing/userservice/src/main/java/com/itemsharing/userservice/UserserviceApplication.java
@@ -1,12 +1,45 @@
 package com.itemsharing.userservice;
 
+import com.itemsharing.userservice.model.Role;
+import com.itemsharing.userservice.model.User;
+import com.itemsharing.userservice.model.UserRole;
+import com.itemsharing.userservice.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @SpringBootApplication
-public class UserserviceApplication {
+public class UserserviceApplication implements CommandLineRunner {
+
+	private UserService userService;
+
+	public UserserviceApplication(UserService userService) {
+		this.userService = userService;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserserviceApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		User user1 = new User();
+		user1.setFirstName("Serhii");
+		user1.setLastName("On");
+		user1.setUsername("SerhiiOn");
+		user1.setPassword("12345");
+		user1.setEmail("email@mail.com");
+
+		Set<UserRole> userRoles = new HashSet<>();
+		Role role1 = new Role();
+		role1.setRoleId(1);
+		role1.setName("ROLE_USER");
+		userRoles.add(new UserRole(user1, role1));
+
+		userService.createUser(user1);
 	}
 }
