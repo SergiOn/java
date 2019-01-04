2089fa0f06ad0f912a7a91d649f1c1b6d4968e47
SergiOn <onishhenko@gmail.com>
2019-01-04 14:20:11 +0200

    #spring-liquibase-okr # created eureka server

---
+++ spring-liquibase-okr/eurekaservice/src/main/java/com/springliquibase/eurekaservice/EurekaserviceApplication.java
@@ -0,0 +1,16 @@
+package com.springliquibase.eurekaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaserviceApplication.class, args);
+	}
+
+}
+
