983c20a6348320e8eba85c6a3791bea3360b8cfc
SergiOn <onishhenko@gmail.com>
2019-01-04 16:44:26 +0200

    #spring-liquibase-okr # created zipkin service

---
+++ spring-liquibase-okr/zipkinservice/src/main/java/com/springliquibase/zipkinservice/ZipkinserviceApplication.java
@@ -0,0 +1,17 @@
+package com.springliquibase.zipkinservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin2.server.internal.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableZipkinServer
+@EnableEurekaClient
+public class ZipkinserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinserviceApplication.class, args);
+	}
+
+}
