01db70c445673ce631ca0a042b1f2ac058bea5ac
SergiOn <onishhenko@gmail.com>
2019-01-04 20:39:06 +0200

    #spring-liquibase-okr # updated authorization service entity user file and log file

--- spring-liquibase-okr/authorizationservice/src/main/java/com/springliquibase/authorizationservice/model/User.java
+++ spring-liquibase-okr/authorizationservice/src/main/java/com/springliquibase/authorizationservice/model/User.java
@@ -1,16 +1,27 @@
 package com.springliquibase.authorizationservice.model;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Collection;
 
 @Entity
+@Getter
+@Setter
 public class User implements UserDetails {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
+    public String getUsername() {
         return null;
     }
 
@@ -20,7 +31,7 @@ public class User implements UserDetails {
     }
 
     @Override
-    public String getUsername() {
+    public Collection<? extends GrantedAuthority> getAuthorities() {
         return null;
     }
 
