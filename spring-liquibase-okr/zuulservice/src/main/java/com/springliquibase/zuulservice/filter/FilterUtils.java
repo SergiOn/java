e5a58fd35c36665defeb862fcf93247cd194facc
SergiOn <onishhenko@gmail.com>
2019-01-08 02:24:27 +0200

    #spring-liquibase-okr # updated zuul filter constants

--- spring-liquibase-okr/zuulservice/src/main/java/com/springliquibase/zuulservice/filter/FilterUtils.java
+++ spring-liquibase-okr/zuulservice/src/main/java/com/springliquibase/zuulservice/filter/FilterUtils.java
@@ -3,12 +3,14 @@ package com.springliquibase.zuulservice.filter;
 import com.netflix.zuul.context.RequestContext;
 import org.springframework.stereotype.Component;
 
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.*;
+
 @Component
 public class FilterUtils {
     public static final String CORRELATION_ID = "is-correlation-id";
-    public static final String PRE_FILTER_TYPE = "pre";
-    public static final String POST_FILTER_TYPE = "post";
-    public static final String ROUTE_FILTER_TYPE = "route";
+    public static final String PRE_FILTER_TYPE = PRE_TYPE;
+    public static final String POST_FILTER_TYPE = POST_TYPE;
+    public static final String ROUTE_FILTER_TYPE = ROUTE_TYPE;
 
     public String getCorrelationId() {
         RequestContext ctx = RequestContext.getCurrentContext();
