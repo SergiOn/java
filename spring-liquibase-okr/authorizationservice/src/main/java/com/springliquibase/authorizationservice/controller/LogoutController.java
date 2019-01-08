3812e40aa16dd50118bcb4f0ce7a9d4b5031069d
SergiOn <onishhenko@gmail.com>
2019-01-08 02:00:55 +0200

    #spring-liquibase-okr # updated logs

---
+++ spring-liquibase-okr/authorizationservice/src/main/java/com/springliquibase/authorizationservice/controller/LogoutController.java
@@ -0,0 +1,9 @@
+package com.springliquibase.authorizationservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping({"/logout", "sign-out"})
+public class LogoutController {
+}
