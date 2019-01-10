872f6d7292b62a25384266f9e629b65507616175
SergiOn <onishhenko@gmail.com>
2019-01-11 01:13:19 +0200

    #spring-liquibase-okr # deleted unused logic in authorization service

--- spring-liquibase-okr/authorizationservice/src/main/java/com/springliquibase/authorizationservice/security/JWTOAuth2Config.java
+++ spring-liquibase-okr/authorizationservice/src/main/java/com/springliquibase/authorizationservice/security/JWTOAuth2Config.java
@@ -6,11 +6,8 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenEnhancer;
-import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-
-import java.util.Arrays;
 
 @Configuration
 @AllArgsConstructor
@@ -19,15 +16,11 @@ public class JWTOAuth2Config extends AuthorizationServerConfigurerAdapter {
     private final AuthenticationManager authenticationManager;
     private final UserSecurityService userDetailsService;
     private final TokenStore tokenStore;
-//    private final DefaultTokenServices tokenServices;
     private final JwtAccessTokenConverter jwtAccessTokenConverter;
     private final TokenEnhancer jwtTokenEnhancer;
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
-        tokenEnhancerChain.setTokenEnhancers(Arrays.asList(jwtTokenEnhancer, jwtAccessTokenConverter));
-
         endpoints.tokenStore(tokenStore)
                 .accessTokenConverter(jwtAccessTokenConverter)
                 .authenticationManager(authenticationManager)
--- spring-liquibase-okr/authorizationservice/src/main/java/com/springliquibase/authorizationservice/security/JWTTokenStoreConfig.java
+++ spring-liquibase-okr/authorizationservice/src/main/java/com/springliquibase/authorizationservice/security/JWTTokenStoreConfig.java
@@ -1,16 +1,17 @@
 package com.springliquibase.authorizationservice.security;
 
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
-import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
+@AllArgsConstructor
 public class JWTTokenStoreConfig {
 
     @Value("${oauth2.signing.key}")
@@ -37,11 +38,6 @@ public class JWTTokenStoreConfig {
         converter.setSigningKey(jwtSigningKey);
 
         return converter;
-    }
-
-    @Bean
-    public TokenEnhancer jwtTokenEnhancer() {
-        return new JWTTokenEnhancer();
     }
 
 }
