74526342f388ea5121a0c58441f10c1cc571b4bf
SergiOn <onishhenko@gmail.com>
2019-05-11 15:20:47 +0300

    #spring-hibernate-tutorial # section 4, lecture 19-20 # Code Demo - Rough Prototype Part

---
+++ Spring-Hibernate-for-Beginners-includes-Spring-Boot/spring-demo-one/src/com/luv2code/springdemo/MyApp.java
@@ -0,0 +1,12 @@
+package com.luv2code.springdemo;
+
+public class MyApp {
+
+    public static void main(String[] args) {
+
+//        Coach theCoach = new BaseballCoach();
+        Coach theCoach = new TrackCoach();
+
+        System.out.println(theCoach.getDailyWorkout());
+    }
+}
---
+++ Spring-Hibernate-for-Beginners-includes-Spring-Boot/spring-demo-one/src/com/luv2code/springdemo/TrackCoach.java
@@ -0,0 +1,9 @@
+package com.luv2code.springdemo;
+
+public class TrackCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Run a hard 5k";
+    }
+}
