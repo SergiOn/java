6f98f8e4792dbae4cb7c2fccb687ea2ea597dd01
SergiOn <onishhenko@gmail.com>
2019-05-11 19:28:34 +0300

    #spring-hibernate-tutorial # section 4, lecture 27 # Practice Activity #1 - Inversion of Control with XML Configuration

---
+++ Spring-Hibernate-for-Beginners-includes-Spring-Boot/spring-demo-one/src/com/luv2code/springdemo/FootballCoach.java
@@ -0,0 +1,9 @@
+package com.luv2code.springdemo;
+
+public class FootballCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Let's focus on winning the Champions League";
+    }
+}
