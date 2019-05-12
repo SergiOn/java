797fc81bdd3eb7a27c6c45aa65a84d9e1ee01dd4
SergiOn <onishhenko@gmail.com>
2019-05-13 00:01:03 +0300

    #spring-hibernate-tutorial # section 6, lecture 48 # Bean Lifecycle - Write Some Code

--- Spring-Hibernate-for-Beginners-includes-Spring-Boot/spring-demo-one/src/com/luv2code/springdemo/TrackCoach.java
+++ Spring-Hibernate-for-Beginners-includes-Spring-Boot/spring-demo-one/src/com/luv2code/springdemo/TrackCoach.java
@@ -17,4 +17,12 @@ public class TrackCoach implements Coach {
     public String getDailyFortune() {
         return "Just do it: " + fortuneService.getFortune();
     }
+
+    public void doMyStartupStuff() {
+        System.out.println("TrackCoach: init");
+    }
+
+    public void doMyCleanupStuff() {
+        System.out.println("TrackCoach: destroy");
+    }
 }
