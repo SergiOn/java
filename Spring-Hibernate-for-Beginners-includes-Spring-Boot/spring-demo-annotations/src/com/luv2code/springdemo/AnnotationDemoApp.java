49dc48e3d9316ad6a64ca77b7c18615d6aede631
SergiOn <onishhenko@gmail.com>
2019-05-14 20:27:58 +0300

    #spring-hibernate-tutorial # section 9, lecture 79-81 # Bean Lifecycle Method Annotations - Write Some Code

--- Spring-Hibernate-for-Beginners-includes-Spring-Boot/spring-demo-annotations/src/com/luv2code/springdemo/AnnotationDemoApp.java
+++ Spring-Hibernate-for-Beginners-includes-Spring-Boot/spring-demo-annotations/src/com/luv2code/springdemo/AnnotationDemoApp.java
@@ -4,16 +4,25 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class AnnotationDemoApp {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		// read spring config file
+		ClassPathXmlApplicationContext context = 
+				new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		// get the bean from spring container
+		Coach theCoach = context.getBean("tennisCoach", Coach.class);
+		
+		// call a method on the bean
+		System.out.println(theCoach.getDailyWorkout());
 
-        Coach theCoach = context.getBean("thatSillyCoach", Coach.class);
+		// call method to get daily fortune
+		System.out.println(theCoach.getDailyFortune());
+				
+		// close the context
+		context.close();	
+	}
 
-        System.out.println(theCoach.getDailyWorkout());
-        System.out.println(Coach.class);
-
-        context.close();
-
-    }
 }
+
+
