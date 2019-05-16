1ca1a44ac69003b580b2feb8445d1bd0f74c4ab1
SergiOn <onishhenko@gmail.com>
2019-05-16 21:16:30 +0300

    #spring-hibernate-tutorial # section 17, lecture 169 # FAQ: Spring MVC Custom Validation - Possible to validate with multiple strings?

--- Spring-Hibernate-for-Beginners-includes-Spring-Boot/spring-mvc-demo/src/com/luv2code/springdemo/mvc/CustomerController.java
+++ Spring-Hibernate-for-Beginners-includes-Spring-Boot/spring-mvc-demo/src/com/luv2code/springdemo/mvc/CustomerController.java
@@ -1,34 +1,53 @@
 package com.luv2code.springdemo.mvc;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
-    @RequestMapping("/showForm")
-    public String showForm(Model theModel) {
-        theModel.addAttribute("customer", new Customer());
-        return "customer-form";
-    }
-
-    @RequestMapping("/processForm")
-    public String processForm(
-            @Valid @ModelAttribute("customer") Customer theCustomer,
-            BindingResult bindingResult) {
-
-        if (bindingResult.hasErrors()) {
-            return "customer-form";
-        } else {
-            return "customer-confirmation";
-        }
-    }
+	// add an initbinder ... to convert trim input strings
+	// remove leading and trailing whitespace
+	// resolve issue for our validation
+	
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
+	
+	
+	@RequestMapping("/showForm")
+	public String showForm(Model theModel) {
+		
+		theModel.addAttribute("customer", new Customer());
+		
+		return "customer-form";
+	}
+	
+	@RequestMapping("/processForm")
+	public String processForm(
+			@Valid @ModelAttribute("customer") Customer theCustomer,
+			BindingResult theBindingResult) {
+		
+		System.out.println("Last name: |" + theCustomer.getLastName() + "|");
+		
+		if (theBindingResult.hasErrors()) {
+			return "customer-form";
+		}
+		else {
+			return "customer-confirmation";
+		}
+	}
 }
