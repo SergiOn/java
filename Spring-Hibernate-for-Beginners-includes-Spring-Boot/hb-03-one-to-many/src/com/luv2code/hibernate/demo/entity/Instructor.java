010aef00442270d558d296a31e7a8b2f212ce9bf
SergiOn <onishhenko@gmail.com>
2019-05-23 23:29:08 +0300

    #spring-hibernate-tutorial # section 24, lecture 227 # @OneToMany - Bi-Directional - Update Instructor

--- Spring-Hibernate-for-Beginners-includes-Spring-Boot/hb-03-one-to-many/src/com/luv2code/hibernate/demo/entity/Instructor.java
+++ Spring-Hibernate-for-Beginners-includes-Spring-Boot/hb-03-one-to-many/src/com/luv2code/hibernate/demo/entity/Instructor.java
@@ -1,6 +1,8 @@
 package com.luv2code.hibernate.demo.entity;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "instructor")
@@ -23,6 +25,9 @@ public class Instructor {
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "instructor_detail_id")
     private InstructorDetail instructorDetail;
+
+    @OneToMany(mappedBy = "instructor", cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    private List<Course> courses;
 
     public Instructor() {
     }
@@ -72,6 +77,25 @@ public class Instructor {
 
     public void setInstructorDetail(InstructorDetail instructorDetail) {
         this.instructorDetail = instructorDetail;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+
+    public void add(Course tempCourse) {
+
+        if (courses == null) {
+            courses = new ArrayList<>();
+        }
+
+        courses.add(tempCourse);
+
+        tempCourse.setInstructor(this);
     }
 
     @Override
