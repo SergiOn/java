d4307f1c277f0d1144faa8d73a47f6222f718d15
SergiOn <onishhenko@gmail.com>
2019-05-23 23:49:56 +0300

    #spring-hibernate-tutorial # section 24, lecture 230 # @OneToMany - Bi-Directional - Retrieve Instructor Courses

---
+++ Spring-Hibernate-for-Beginners-includes-Spring-Boot/hb-03-one-to-many/src/com/luv2code/hibernate/demo/GetInstructorCoursesDemo.java
@@ -0,0 +1,39 @@
+package com.luv2code.hibernate.demo;
+
+import com.luv2code.hibernate.demo.entity.Course;
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.Instructor2;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class GetInstructorCoursesDemo {
+
+    public static void main(String[] args) {
+
+        SessionFactory factory = new Configuration()
+                                    .configure("hibernate.cfg.xml")
+                                    .addAnnotatedClass(Instructor.class)
+                                    .addAnnotatedClass(InstructorDetail.class)
+                                    .addAnnotatedClass(Course.class)
+                                    .buildSessionFactory();
+
+        Session session = factory.getCurrentSession();
+
+        try {
+
+            session.beginTransaction();
+
+            Instructor tempInstructor = session.get(Instructor.class, 1);
+
+            System.out.println("Courses: " + tempInstructor.getCourses());
+
+            session.getTransaction().commit();
+
+        } finally {
+            session.close();
+            factory.close();
+        }
+    }
+}
