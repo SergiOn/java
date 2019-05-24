b14a3c495cd1bd5f54c36525bb5097535e694f89
SergiOn <onishhenko@gmail.com>
2019-05-25 00:40:23 +0300

    #spring-hibernate-tutorial # section 25, lecture 235 # Eager vs Lazy Loading - Coding - Lazy

--- Spring-Hibernate-for-Beginners-includes-Spring-Boot/hb-eager-vs-lazy-demo/src/com/luv2code/hibernate/demo/entity/Instructor.java
+++ Spring-Hibernate-for-Beginners-includes-Spring-Boot/hb-eager-vs-lazy-demo/src/com/luv2code/hibernate/demo/entity/Instructor.java
@@ -26,7 +26,8 @@ public class Instructor {
     @JoinColumn(name = "instructor_detail_id")
     private InstructorDetail instructorDetail;
 
-    @OneToMany(fetch = FetchType.EAGER,
+//    @OneToMany(fetch = FetchType.EAGER,
+    @OneToMany(fetch = FetchType.LAZY,
                mappedBy = "instructor",
                cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
     private List<Course> courses;
