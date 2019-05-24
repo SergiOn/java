b109c4a644327ff02047c2e985ef92c75643fad5
SergiOn <onishhenko@gmail.com>
2019-05-25 00:22:27 +0300

    #spring-hibernate-tutorial # section 25, lecture 234 # Eager vs Lazy Loading - Coding - Eager

---
+++ Spring-Hibernate-for-Beginners-includes-Spring-Boot/hb-eager-vs-lazy-demo/src/com/luv2code/hibernate/demo/EagerLazyDemo.java
@@ -0,0 +1,40 @@
+package com.luv2code.hibernate.demo;
+
+import com.luv2code.hibernate.demo.entity.Course;
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class EagerLazyDemo {
+
+    public static void main(String[] args) {
+
+        SessionFactory factory = new Configuration()
+                                    .configure("hibernate.cfg.xml")
+                                    .addAnnotatedClass(Instructor.class)
+                                    .addAnnotatedClass(InstructorDetail.class)
+                                    .addAnnotatedClass(Course.class)
+                                    .buildSessionFactory();
+
+        Session session = factory.getCurrentSession();
+
+        try {
+
+            session.beginTransaction();
+
+            Instructor tempInstructor = session.get(Instructor.class, 1);
+
+            System.out.println("Instructor: " + tempInstructor);
+
+            System.out.println("Courses: " + tempInstructor.getCourses());
+
+            session.getTransaction().commit();
+
+        } finally {
+            session.close();
+            factory.close();
+        }
+    }
+}
