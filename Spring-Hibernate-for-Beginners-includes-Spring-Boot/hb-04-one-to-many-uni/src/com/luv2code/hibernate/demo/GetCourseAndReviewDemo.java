c936d2e1ad21f5fcf20c1d4bd44095b3462f052d
SergiOn <onishhenko@gmail.com>
2019-05-25 13:13:38 +0300

    #spring-hibernate-tutorial # section 26, lecture 247 # @OneToMany - Uni-Directional - Get Course Reviews

---
+++ Spring-Hibernate-for-Beginners-includes-Spring-Boot/hb-04-one-to-many-uni/src/com/luv2code/hibernate/demo/GetCourseAndReviewDemo.java
@@ -0,0 +1,41 @@
+package com.luv2code.hibernate.demo;
+
+import com.luv2code.hibernate.demo.entity.Course;
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+import com.luv2code.hibernate.demo.entity.Review;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class GetCourseAndReviewDemo {
+
+    public static void main(String[] args) {
+
+        SessionFactory factory = new Configuration()
+                                    .configure("hibernate.cfg.xml")
+                                    .addAnnotatedClass(Instructor.class)
+                                    .addAnnotatedClass(InstructorDetail.class)
+                                    .addAnnotatedClass(Course.class)
+                                    .addAnnotatedClass(Review.class)
+                                    .buildSessionFactory();
+
+        Session session = factory.getCurrentSession();
+
+        try {
+
+            session.beginTransaction();
+
+            Course course = session.get(Course.class, 10);
+
+            System.out.println("Course: " + course);
+            System.out.println("Review: " + course.getReviews());
+
+            session.getTransaction().commit();
+
+        } finally {
+            session.close();
+            factory.close();
+        }
+    }
+}
