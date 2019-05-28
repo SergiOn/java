b3c7ac1bb4c22e463fd3d38c2b0cac3186b1ff51
SergiOn <onishhenko@gmail.com>
2019-05-28 13:58:45 +0300

    #spring-hibernate-tutorial # section 36, lecture 325 # AOP - Pointcut Expressions - Match any Return Type

--- Spring-Hibernate-for-Beginners-includes-Spring-Boot/spring-demo-aop/src/com/luv2code/aopdemo/MainDemoApp.java
+++ Spring-Hibernate-for-Beginners-includes-Spring-Boot/spring-demo-aop/src/com/luv2code/aopdemo/MainDemoApp.java
@@ -3,6 +3,7 @@ package com.luv2code.aopdemo;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.luv2code.aopdemo.dao.AccountDAO;
+import com.luv2code.aopdemo.dao.MembershipDAO;
 
 public class MainDemoApp {
 
@@ -15,14 +16,15 @@ public class MainDemoApp {
 		// get the bean from spring container
 		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
 
+		// get membership bean from spring container
+		MembershipDAO theMembershipDAO = 
+				context.getBean("membershipDAO", MembershipDAO.class);
+
 		// call the business method
 		theAccountDAO.addAccount();
 
-		// do it again!
-		System.out.println("\nlet's call it again!\n");
-
-		// call the business method again
-		theAccountDAO.addAccount();
+		// call the membership business method
+		theMembershipDAO.addSillyMember();
 
 		// close the context
 		context.close();
