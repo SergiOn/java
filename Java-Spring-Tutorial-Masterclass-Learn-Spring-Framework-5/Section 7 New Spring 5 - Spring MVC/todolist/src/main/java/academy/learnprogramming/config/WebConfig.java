c2b85fe68140e50824bbff92fefe3e154c97e561
SergiOn <onishhenko@gmail.com>
2019-06-03 23:57:48 +0300

    #java-spring-framework-masterclass # section 7, lecture 54 # Setup Dispatcher Servlet

---
+++ Java-Spring-Tutorial-Masterclass-Learn-Spring-Framework-5/Section 7 New Spring 5 - Spring MVC/todolist/src/main/java/academy/learnprogramming/config/WebConfig.java
@@ -0,0 +1,11 @@
+package academy.learnprogramming.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "academy.learnprogramming")
+public class WebConfig {
+}
