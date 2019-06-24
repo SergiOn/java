90c05420ce47a07bf2eef165d7b948125918802c
SergiOn <onishhenko@gmail.com>
2019-06-24 20:40:51 +0300

    #rabbitmq-messaging-with-java-spring # section 5, lecture 26 # Direct Exchange Configuration with Spring AMQP

---
+++ Learn-RabbitMQ-Asynchronous-Messaging-with-Java-and-Spring/rabbitmqlistener/src/main/java/com/rabbitmq/rabbitmqlistener/RabbitMQExchangeConfiguration.java
@@ -0,0 +1,24 @@
+package com.rabbitmq.rabbitmqlistener;
+
+import org.springframework.amqp.core.Exchange;
+import org.springframework.amqp.core.ExchangeBuilder;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQExchangeConfiguration {
+
+    @Bean
+    Exchange exampleExchange() {
+        return new TopicExchange("ExampleExchange");
+    }
+
+    @Bean
+    Exchange example2ndExchange() {
+        return ExchangeBuilder.directExchange("Example2ndExchange")
+                .autoDelete()
+                .internal()
+                .build();
+    }
+}
