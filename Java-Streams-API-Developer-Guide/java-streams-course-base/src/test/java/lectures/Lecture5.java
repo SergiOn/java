a0ed050556f41db94a54e3ddfd4153019caeddcb
SergiOn <onishhenko@gmail.com>
2019-06-30 14:59:09 +0300

    #java-streams # section 7, lecture 20, 23 # Filtering and Transformations

--- Java-Streams-API-Developer-Guide/java-streams-course-base/src/test/java/lectures/Lecture5.java
+++ Java-Streams-API-Developer-Guide/java-streams-course-base/src/test/java/lectures/Lecture5.java
@@ -8,6 +8,7 @@ import beans.Person;
 import beans.PersonDTO;
 import com.google.common.collect.ImmutableList;
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import mockdata.MockData;
@@ -15,10 +16,18 @@ import org.junit.Test;
 
 public class Lecture5 {
 
+  final Predicate<Car> carPredicate = car -> car.getPrice() < 10000;
+
   @Test
   public void understandingFilter() throws Exception {
     ImmutableList<Car> cars = MockData.getCars();
 
+    List<Car> carsFiltered = cars.stream()
+//            .filter(car -> car.getPrice() < 10000)
+            .filter(carPredicate)
+            .collect(Collectors.toList());
+
+    assertThat(carsFiltered).isNotEmpty();
   }
 
   @Test
@@ -26,12 +35,36 @@ public class Lecture5 {
     // transform from one data type to another
     List<Person> people = MockData.getPeople();
 
+    List<PersonDTO> personDTOs = people.stream()
+//            .map(person -> new PersonDTO(person.getId(), person.getFirstName(), person.getAge()))
+            .map(PersonDTO::map)
+            .collect(Collectors.toList());
+
+    assertThat(people).hasSize(personDTOs.size());
+    assertThat(personDTOs).hasSize(1000);
+
+
+    Function<Person, PersonDTO> personToPersonDTO = person -> new PersonDTO(person.getId(), person.getFirstName(), person.getAge());
+
+    List<PersonDTO> dtos = people.stream()
+            .map(personToPersonDTO)
+            .collect(Collectors.toList());
+
+    assertThat(people).hasSize(dtos.size());
+    assertThat(dtos).hasSize(1000);
   }
 
   @Test
   public void averageCarPrice() throws Exception {
     // calculate average of car prices
+    ImmutableList<Car> cars = MockData.getCars();
 
+    double average = cars.stream()
+            .mapToDouble(Car::getPrice)
+            .average()
+            .orElse(0);
+
+    System.out.println(average);
   }
 
   @Test
