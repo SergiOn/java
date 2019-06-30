364fee4e6067235ca84449da21ac999313642589
SergiOn <onishhenko@gmail.com>
2019-06-30 19:21:55 +0300

    #java-streams # section 12, lecture 38 - 40 # Joining Strings

--- Java-Streams-API-Developer-Guide/java-streams-course-base/src/test/java/lectures/Lecture11.java
+++ Java-Streams-API-Developer-Guide/java-streams-course-base/src/test/java/lectures/Lecture11.java
@@ -12,11 +12,35 @@ public class Lecture11 {
   public void joiningStrings() throws Exception {
     List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
 
+    StringBuilder join = new StringBuilder();
+//    String join = "";
+    String separator = "";
+
+    for (String name : names) {
+
+      join.append(separator).append(name);
+//      join += separator + name;
+      separator = ", ";
+    }
+
+    System.out.println("join: " + join);
   }
 
   @Test
   public void joiningStringsWithStream() throws Exception {
     List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
 
+    String join = names.stream()
+            .collect(Collectors.joining(", "));
+
+    String join2 = String.join(", ", names);
+
+    String join3 = names.stream()
+            .map(String::toUpperCase)
+            .collect(Collectors.joining(" | "));
+
+    System.out.println("join: " + join);
+    System.out.println("join2: " + join2);
+    System.out.println("join3: " + join3);
   }
 }
