45a7a60d029391115639fba491775601c2d2066f
SergiOn <onishhenko@gmail.com>
2019-06-30 16:54:24 +0300

    #java-streams # section 8, lecture 24 - 26 # Find Any vs Find First

--- Java-Streams-API-Developer-Guide/java-streams-course-base/src/test/java/lectures/Lecture6.java
+++ Java-Streams-API-Developer-Guide/java-streams-course-base/src/test/java/lectures/Lecture6.java
@@ -4,7 +4,11 @@ package lectures;
 import com.google.common.collect.Lists;
 import java.util.Arrays;
 import java.util.function.Predicate;
+import java.util.stream.Stream;
+
 import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class Lecture6 {
 
@@ -14,12 +18,26 @@ public class Lecture6 {
   public void findAny() throws Exception {
     Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
+    int integer = Arrays.stream(numbers)
+//            .parallel()
+            .filter(i -> i < 10)
+            .findAny()
+            .get();
+
+    assertThat(integer).isEqualTo(1);
   }
 
   @Test
   public void findFirst() throws Exception {
     Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
+    int integer = Arrays.stream(numbers)
+//            .parallel()
+            .filter(i -> i < 10)
+            .findFirst()
+            .get();
+
+    assertThat(integer).isEqualTo(1);
   }
 }
 
