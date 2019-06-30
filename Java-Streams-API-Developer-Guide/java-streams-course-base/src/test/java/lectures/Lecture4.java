b28e26557a286471360204cb44d870b873f35255
SergiOn <onishhenko@gmail.com>
2019-06-30 14:34:40 +0300

    #java-streams # section 6, lecture 18, 19 # Distinct and Collectors.toSet()

--- Java-Streams-API-Developer-Guide/java-streams-course-base/src/test/java/lectures/Lecture4.java
+++ Java-Streams-API-Developer-Guide/java-streams-course-base/src/test/java/lectures/Lecture4.java
@@ -15,11 +15,20 @@ public class Lecture4 {
   public void distinct() throws Exception {
     final List<Integer> numbers = ImmutableList.of(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 9, 9, 9);
 
+    List<Integer> collect = numbers.stream()
+            .distinct()
+            .collect(Collectors.toList());
+
+    assertThat(collect).isEqualTo(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9));
+    assertThat(collect).hasSize(9);
   }
 
   @Test
   public void distinctWithSet() throws Exception {
     final List<Integer> numbers = ImmutableList.of(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 9, 9, 9);
 
+    Set<Integer> collect = numbers.stream().collect(Collectors.toSet());
+
+    assertThat(collect).isEqualTo(Set.of(1, 2, 3, 4, 5, 6, 7, 8, 9));
   }
 }
