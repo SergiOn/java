ca32706faf50bb34d5adf6fdf53767e7fe875bbd
SergiOn <onishhenko@gmail.com>
2019-06-30 14:25:36 +0300

    #java-streams # section 5, lecture 15 - 17 # Max, Min & Comparators

--- Java-Streams-API-Developer-Guide/java-streams-course-base/src/test/java/lectures/Lecture3.java
+++ Java-Streams-API-Developer-Guide/java-streams-course-base/src/test/java/lectures/Lecture3.java
@@ -13,11 +13,25 @@ public class Lecture3 {
   public void min() throws Exception {
     final List<Integer> numbers = ImmutableList.of(1, 2, 3, 100, 23, 93, 99);
 
+    int integer = numbers.stream()
+            .min(Integer::compareTo)
+//            .min(Comparator.naturalOrder())
+//            .min((number1, number2) -> number1 > number2 ? 1 : -1)
+            .get();
+    System.out.println("min: " + integer);
+
+    assertThat(integer).isEqualTo(1);
   }
 
   @Test
   public void max() throws Exception {
     final List<Integer> numbers = ImmutableList.of(1, 2, 3, 100, 23, 93, 99);
 
+    int integer = numbers.stream()
+            .max(Integer::compareTo)
+            .get();
+    System.out.println("max: " + integer);
+
+    assertThat(integer).isEqualTo(100);
   }
 }
