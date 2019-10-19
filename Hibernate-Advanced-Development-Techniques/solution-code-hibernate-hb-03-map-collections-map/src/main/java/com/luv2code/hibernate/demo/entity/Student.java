b9b6fe2c42d153149cf1b6f32658efe6c0e16de5
SergiOn <onishhenko@gmail.com>
2019-10-19 23:32:52 +0300

    #hibernate-tutorial-advanced # section 3, lecture 14 # Maps - Write Some Code

---
+++ Hibernate-Advanced-Development-Techniques/solution-code-hibernate-hb-03-map-collections-map/src/main/java/com/luv2code/hibernate/demo/entity/Student.java
@@ -0,0 +1,86 @@
+package com.luv2code.hibernate.demo.entity;
+
+import javax.persistence.*;
+import java.util.HashMap;
+import java.util.Map;
+
+@Entity
+@Table(name = "student")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @ElementCollection
+    @CollectionTable(name = "image")
+    @MapKeyColumn(name = "file_name")
+    @Column(name = "image_name")
+    private Map<String, String> images = new HashMap<>();
+
+    public Student(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Map<String, String> getImages() {
+        return images;
+    }
+
+    public void setImages(Map<String, String> images) {
+        this.images = images;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                    "id=" + id + ", " +
+                    "firstName='" + firstName + '\'' + ", " +
+                    "lastName='" + lastName + '\'' + ", " +
+                    "email='" + email + '\'' +
+                '}';
+    }
+}
