abf29beaea52f006bb321e4dea3c300b86593752
SergiOn <onishhenko@gmail.com>
2019-10-20 12:38:22 +0300

    #hibernate-tutorial-advanced # section 3, lecture 17 # Sorted Sets - Write Some Code - Part 2

--- Hibernate-Advanced-Development-Techniques/solution-code-hibernate-hb-04-map-collections-sortedset/src/main/java/com/luv2code/hibernate/demo/entity/Student.java
+++ Hibernate-Advanced-Development-Techniques/solution-code-hibernate-hb-04-map-collections-sortedset/src/main/java/com/luv2code/hibernate/demo/entity/Student.java
@@ -24,10 +24,14 @@ public class Student {
 
     @ElementCollection
     @CollectionTable(name = "image")
-//    @OrderBy // only one possible order; file_name ask
-    @org.hibernate.annotations.OrderBy(clause = "file_name desc") // default ask
+//    @OrderBy // only one possible order; file_name asc
+    @org.hibernate.annotations.OrderBy(clause = "file_name desc") // default asc
+//    @OrderBy(value = "file_name desc") // default asc
     @Column(name = "file_name")
     private Set<String> images = new LinkedHashSet<>();
+
+    public Student() {
+    }
 
     public Student(String firstName, String lastName, String email) {
         this.firstName = firstName;
