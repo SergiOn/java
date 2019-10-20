a00e46f499872ed9889dff735e795870efaa4410
SergiOn <onishhenko@gmail.com>
2019-10-20 12:27:19 +0300

    #hibernate-tutorial-advanced # section 3, lecture 16 # Sorted Sets - Write Some Code - Part 1

---
+++ Hibernate-Advanced-Development-Techniques/solution-code-hibernate-hb-04-map-collections-sortedset/src/main/java/com/luv2code/hibernate/demo/entity/Student.java
@@ -0,0 +1,87 @@
+package com.luv2code.hibernate.demo.entity;
+
+import javax.persistence.*;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "student")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @ElementCollection
+    @CollectionTable(name = "image")
+//    @OrderBy // only one possible order; file_name ask
+    @org.hibernate.annotations.OrderBy(clause = "file_name desc") // default ask
+    @Column(name = "file_name")
+    private Set<String> images = new LinkedHashSet<>();
+
+    public Student(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<String> getImages() {
+        return images;
+    }
+
+    public void setImages(Set<String> images) {
+        this.images = images;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id + ", " +
+                "firstName='" + firstName + '\'' + ", " +
+                "lastName='" + lastName + '\'' + ", " +
+                "email='" + email + '\'' +
+                '}';
+    }
+}
