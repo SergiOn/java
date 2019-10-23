6ccd60213666288f089114e82f14e466b34b28b6
SergiOn <onishhenko@gmail.com>
2019-10-23 17:43:58 +0300

    #hibernate-tutorial-advanced # section 6, lecture 28 # Inheritance Mapping - Single Table - Coding - Part 1

---
+++ Hibernate-Advanced-Development-Techniques/solution-code-hibernate-hb-inheritance-01-singletable/src/main/java/com/luv2code/hibernate/demo/CreateUserStudentInstructorDemo.java
@@ -0,0 +1,49 @@
+package com.luv2code.hibernate.demo;
+
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.Student;
+import com.luv2code.hibernate.demo.entity.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class CreateUserStudentInstructorDemo {
+
+    public static void main(String[] args) {
+
+        // create session factory
+        SessionFactory factory = new Configuration()
+                                    .configure("hibernate.cfg.xml")
+                                    .addAnnotatedClass(Student.class)
+                                    .addAnnotatedClass(Instructor.class)
+                                    .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+            // create the object
+            Student tempStudent = new Student("John", "Doe", "john@luv2code.com", "Hibernate");
+            Instructor tempInstructor = new Instructor("Marry", "Public", "mary@luv2code.com", 20000.00);
+
+            // start the transaction
+            session.beginTransaction();
+
+            // save the object
+            System.out.println("Saving the student and instructor...");
+            session.persist(tempStudent);
+            session.persist(tempInstructor);
+
+            // commit the transaction
+            session.getTransaction().commit();
+            System.out.println("Done!!");
+
+        } finally {
+            // clean up code
+            session.close();
+            factory.close();
+        }
+
+    }
+
+}
---
+++ Hibernate-Advanced-Development-Techniques/solution-code-hibernate-hb-inheritance-01-singletable/src/main/java/com/luv2code/hibernate/demo/entity/Instructor.java
@@ -0,0 +1,22 @@
+package com.luv2code.hibernate.demo.entity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Instructor extends User {
+
+    private double salary;
+
+    public Instructor(String firstName, String lastName, String email, double salary) {
+        super(firstName, lastName, email);
+        this.salary = salary;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
