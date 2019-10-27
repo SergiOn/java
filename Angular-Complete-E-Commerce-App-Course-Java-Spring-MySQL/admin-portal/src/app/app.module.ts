9f9fa263aaec8837298fae4e654d75f5c59c0b40
SergiOn <onishhenko@gmail.com>
2019-10-27 15:31:52 +0200

    #e-commerce-app-angular-java-spring-mysql # section 2, lecture 8 # Add Nav-Bar Component

--- Angular-Complete-E-Commerce-App-Course-Java-Spring-MySQL/admin-portal/src/app/app.module.ts
+++ Angular-Complete-E-Commerce-App-Course-Java-Spring-MySQL/admin-portal/src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+  ],
+  declarations: [
+    AppComponent,
+    NavBarComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
