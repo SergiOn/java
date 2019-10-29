fc81ddbeb559ec681929ce6ea80ce1b16b952676
SergiOn <onishhenko@gmail.com>
2019-10-29 18:11:12 +0200

    #e-commerce-app-angular-java-spring-mysql # section 3, lecture 16 # Adding Security Utility

--- Angular-Complete-E-Commerce-App-Course-Java-Spring-MySQL/bookstore/src/main/java/com/bookstore/config/SecurityConfig.java
+++ Angular-Complete-E-Commerce-App-Course-Java-Spring-MySQL/bookstore/src/main/java/com/bookstore/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
@@ -17,7 +18,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private Environment environment;
 
     @Autowired
-    private UserSecurityService userSecurityService;
+    private UserDetailsService userSecurityService;
+//    private UserSecurityService userSecurityService;
 
     private BCryptPasswordEncoder passwordEncoder() {
         return SecurityUtility.passwordEncoder();
---
+++ Angular-Complete-E-Commerce-App-Course-Java-Spring-MySQL/bookstore/src/main/java/com/bookstore/service/UserSecurityService.java
@@ -0,0 +1,27 @@
+package com.bookstore.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Optional;
+
+public class UserSecurityService implements UserDetailsService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(UserSecurityService.class);
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return Optional.ofNullable(userRepository.findByUsername(username))
+                .orElseThrow(() -> {
+                    LOG.warn("Username {} not found", username);
+                    throw new UsernameNotFoundException("Username " + username + "not found");
+                });
+    }
+}
