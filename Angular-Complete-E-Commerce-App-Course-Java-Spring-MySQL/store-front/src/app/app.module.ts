719a304e73a01903c23b50e9da6ac0a69cfcebf3
SergiOn <onishhenko@gmail.com>
2019-11-15 15:18:48 +0200

    #e-commerce-app-angular-java-spring-mysql # section 6, lecture 42 # Setup Folder Structure

--- Angular-Complete-E-Commerce-App-Course-Java-Spring-MySQL/store-front/src/app/app.module.ts
+++ Angular-Complete-E-Commerce-App-Course-Java-Spring-MySQL/store-front/src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    HomeComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
