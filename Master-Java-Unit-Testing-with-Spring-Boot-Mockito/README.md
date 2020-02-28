be4b355365b2e9864f5f7d6a95f8e48c285df9e5
SergiOn <onishhenko@gmail.com>
2020-02-28 14:50:10 +0200

    #learn-unit-testing-with-spring-boot # added README.md for current directory and added projects

---
+++ Master-Java-Unit-Testing-with-Spring-Boot-Mockito/README.md
@@ -0,0 +1,12 @@
+## Master Java Unit Testing with Spring Boot & Mockito
+
+### Repository
+
+https://github.com/in28minutes/spring-unit-testing-with-junit-and-mockito
+
+https://github.com/in28minutes/getting-started-in-5-steps
+
+https://github.com/in28minutes/getting-started-in-5-steps/tree/master/spring-unit-testing-with-mockito
+
+https://github.com/in28minutes/getting-started-in-5-steps/tree/master/spring-unit-testing-with-junit-and-mockito-old
+
---
+++ Master-Java-Unit-Testing-with-Spring-Boot-Mockito/spring-unit-testing-with-junit-and-mockito/src/test/java/com/in28minutes/unittesting/unittesting/UnitTestingApplicationTests.java
@@ -0,0 +1,17 @@
+package com.in28minutes.unittesting.unittesting;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+//@TestPropertySource(locations= {"classpath:test-configuration.properties"})
+public class UnitTestingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
