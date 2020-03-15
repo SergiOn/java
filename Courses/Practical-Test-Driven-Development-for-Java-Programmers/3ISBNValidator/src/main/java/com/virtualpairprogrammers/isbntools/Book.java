bda433a5a1c730e902b7255a20df90a662350f42
SergiOn <onishhenko@gmail.com>
2020-03-16 01:07:12 +0200

    #practical-test-driven-development-for-java-programmers # section 7: Chapter 7 - Stubs

---
+++ Courses/Practical-Test-Driven-Development-for-Java-Programmers/3ISBNValidator/src/main/java/com/virtualpairprogrammers/isbntools/Book.java
@@ -0,0 +1,27 @@
+package com.virtualpairprogrammers.isbntools;
+
+public class Book {
+
+    private String isbn;
+    private String title;
+    private String author;
+
+    public Book(String isbn, String title, String author) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+}
---
+++ Courses/Practical-Test-Driven-Development-for-Java-Programmers/3ISBNValidator/src/test/java/com/virtualpairprogrammers/isbntools/StockManagementTests.java
@@ -0,0 +1,27 @@
+package com.virtualpairprogrammers.isbntools;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StockManagementTests {
+
+    @Test
+    public void testCanGetACorrectLocatorCode() {
+        ExternalISBNDataService testService = new ExternalISBNDataService() {
+            @Override
+            public Book lookup(String isbn) {
+                return new Book(isbn, "Of Mice And Men", "J. Steinbeck");
+            }
+        };
+
+        StockManager stockManager = new StockManager();
+        stockManager.setService(testService);
+
+        String isbn = "0140177396";
+        String locatorCode = stockManager.getLocatorCode(isbn);
+
+        assertEquals("7396J4", locatorCode);
+    }
+
+}
